05fb3fa71f56ab9e6308887a8fb4faf092cb4b7d
LucasBilbao <lucasbm789@gmail.com>
2025-04-12 00:06:27 +0400

    feat: implement styles

--- src/app/components/recipe-cards/recipe-card/recipe-card.component.ts
+++ src/app/components/recipe-cards/recipe-card/recipe-card.component.ts
@@ -1,10 +1,11 @@
 import { Component, input } from '@angular/core';
 import { Recipe } from '../../../interfaces/recipe.interface';
 import { TruncatePipe } from '../../../pipes/truncate.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'fl-recipe-card',
-  imports: [TruncatePipe],
+  imports: [TruncatePipe, RouterLink],
   templateUrl: './recipe-card.component.html',
   styleUrl: './recipe-card.component.scss',
 })
